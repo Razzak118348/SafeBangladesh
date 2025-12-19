9e5b92ef9c191298169cc6aa05d0253b4e17c8ba
Razzak118348 <abdurrazzak118348@gmail.com>
2025-12-19 23:35:38 +0600

    context API and h

---
+++ Client/src/Context/ContextApi.jsx
@@ -0,0 +1,103 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut
+} from "firebase/auth";
+import app from "../Firebase/firebase.config.js";
+import { useEffect, useState, createContext } from "react";
+import axios from "axios";
+
+//this is auth context for export auth context api
+export const AuthContext = createContext(null);
+
+//those are social media providers
+const googleProvider = new GoogleAuthProvider();
+// const githubProvider = new GithubAuthProvider();
+
+const ContextApi = ({ children }) => {
+  const auth = getAuth(app);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+  const [allService, setallService] = useState([]);
+
+
+  useEffect(() => {
+    axios.get("https//:backendlink") // your backend endpoint
+      .then(res => setallService(res.data))
+      .catch(err => console.error("Service fetch error:", err));
+  }, []);
+
+  // create user
+  const creatUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // sign in
+  const SignInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // const gitHubLogin = () => {
+  //   setLoading(true);
+  //   return signInWithPopup(auth, githubProvider);
+  // };
+
+  const LogOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // observer
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+
+      if(currentUser?.email){
+        const userData={email:currentUser.email}
+        //give backend link
+        axios.post("https//:localhost backend ",userData)
+        .then(res=>{
+          console.log('token after jwt',res.data)
+
+        })
+        .catch(err=>console.error(err))
+      }
+      setLoading(false);
+    });
+    return () => unSubscribe();
+  }, []);
+
+
+  const authInfo = {
+    user,
+    creatUser,
+    SignInUser,
+    LogOut,
+    loading,
+    googleLogin,
+    // gitHubLogin,
+    allService
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>
+{children}
+    </AuthContext.Provider>
+  );
+};
+
+export default ContextApi;
---
+++ Client/src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+
+const axiosSecure = axios.create({
+    baseURL: '',
+    withCredentials: true
+});
+
+//this return the base backend url(server link) like 'http://localhost:5000' or 'https://api.example.com'
+
+const useAxiosSecure = () => {
+    const navigate = useNavigate()
+    const { LogOut } = useAuth()
+    useEffect(() => {
+        axiosSecure.interceptors.response.use(
+            (response) => {
+                return response;
+            },
+            (error) => {
+                if (error.response.status === 401 || error.response.status === 403) {
+                    console.log('LogOut')
+                    LogOut()
+                        .then(() => {
+                            navigate('/login')
+                        })
+                        .catch((error)=>{console.log(error)});
+                    }
+                }
+    );
+    }, [LogOut, navigate]);
+    return axiosSecure;
+};
+
+export default useAxiosSecure;
