9a2b5ceba1f6e24311e21a773493a5429d44afbe
Razzak118348 <abdurrazzak118348@gmail.com>
2026-01-01 20:35:40 +0600

    update the loading

--- Client/src/Components/Loading/Loading.jsx
+++ Client/src/Components/Loading/Loading.jsx
@@ -1,10 +1,12 @@
 const Loading = () => {
     return (
-        <div className="flex w-52 flex-col gap-4">
+        <div className="flex items-center justify-center">
+            <div className="flex w-52 flex-col gap-6 justify-center items-center">
             <div className="skeleton h-32 w-full"></div>
             <div className="skeleton h-4 w-28"></div>
             <div className="skeleton h-4 w-full"></div>
             <div className="skeleton h-4 w-full"></div>
+        </div>
         </div>
     );
 };
