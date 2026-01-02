99956b78496b81c6a3c0414d3e7058b359ed07e8
Razzak118348 <abdurrazzak118348@gmail.com>
2026-01-03 02:50:25 +0600

    setup the cors

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,28 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const cors =require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors({
+  origin:[
+        'http://localhost:5173',
+"https://safe-bangladesh-org.web.app/"
+  ],
+  Credential:true,
+}
+))
+app.use(express.json());
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('This is Safe Bangladesh Organization server is running!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is running on Port ${port}`)
 })
