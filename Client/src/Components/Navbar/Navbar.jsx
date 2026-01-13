7dfbdc0ba845384ccd2a800fcc0d9c5a1d0c31c3
Razzak118348 <abdurrazzak118348@gmail.com>
2026-01-14 04:09:50 +0600

    add the navbar icon image

--- Client/src/Components/Navbar/Navbar.jsx
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -80,14 +80,14 @@ const {user,LogOut}=useAuth();
       <div className="flex justify-start">
          {/* Logo */}
         <img
-  src="/NBSLogo.svg"
+  src="/favicon.ico"
   alt="logo"
-  className="w-8 md:w-12 h-auto object-contain"
+  className="w-8 md:w-12 h-12 object-contain"
 />
 
         <NavLink
           to="/"
-          className="text-lg md:text-xl pt-2 md:pt-4 font-bold bg-gradient-to-r from-[#3e5a41] via-[#428576] to-[#223d26]
+          className="text-lg md:text-xl pt-2 font-bold bg-gradient-to-r from-[#3e5a41] via-[#428576] to-[#223d26]
           text-transparent bg-clip-text dark:text-white"
         >
           Nirapod Bangladesh Songstha
