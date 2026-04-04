465a7b3836e6572a03ba047fdfea7abfa52147fc
Razzak118348 <abdurrazzak118348@gmail.com>
2026-04-05 00:20:27 +0600

    remove under development title from home page

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -45,9 +45,7 @@ const Home = () => {
 <LatestWork></LatestWork>
 <Gallery></Gallery>
 
-<div className="text-center my-10 text-gray-600 dark:text-gray-300">
-  Website is under development...
-</div>
+
 </div>
 
     </div>
